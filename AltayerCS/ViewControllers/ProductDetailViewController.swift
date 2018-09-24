93c5e897c173dd712c4ae725961f3dd1061e0ea9
Yusuf Miletli <yusuf.miletli@mogumobile.com>
2018-09-24 17:09:02 +0300

    Add architecture groups with folders
    Additional Changes:
    - [ViewControllers][ViewModels] Add related files

---
+++ AltayerCS/ViewControllers/ProductDetailViewController.swift
@@ -0,0 +1,8 @@
+import Foundation
+import UIKit
+
+class ProductDetailViewController: BaseViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
---
+++ AltayerCS/ViewControllers/ProductsViewController.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+
+class ProductsViewController: BaseViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
